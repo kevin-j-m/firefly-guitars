0e16fbe26aff7e79151da77722a9042339d33049
Kevin Murphy <murphy.kevin.mail@gmail.com>
2022-03-25 10:37:53 -0400

    Ruby script to find which FFLP guitars are available.

---
+++ firefly_guitars_for_sale.rb
@@ -0,0 +1,15 @@
+require 'nokogiri'
+require 'open-uri'
+
+doc = Nokogiri::HTML(URI.open("https://guitarsgarden.com/collections/fflp-electric-guitars"))
+
+guitars = doc.search("a").
+  map {|a| a[:href]}.
+  compact.
+  select { |a| a.include?("/collections/fflp-electric-guitars/products") }.
+  uniq.
+  map{ |a| "https://guitarsgarden.com#{a}" }.
+  sort.
+  join("\n")
+
+File.open("available_guitars.txt", "w") { |f| f.write guitars }
