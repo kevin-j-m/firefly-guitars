b9c9d9516a74e7ee075b77440309f93a12d64bea
Kevin Murphy <murphy.kevin.mail@gmail.com>
2024-01-17 15:11:39 -0500

    look for single-cut and s-style, run on schedule

--- firefly_guitars_for_sale.rb
+++ firefly_guitars_for_sale.rb
@@ -3,7 +3,7 @@ require 'open-uri'
 
 doc = Nokogiri::HTML(URI.open("https://guitarsgarden.com/collections/fflp-electric-guitars"))
 
-guitars = doc.search("a").
+lp_guitars = doc.search("a").
   map {|a| a[:href]}.
   compact.
   select { |a| a.include?("/collections/fflp-electric-guitars/products") }.
@@ -11,6 +11,17 @@ guitars = doc.search("a").
   map{ |a| "https://guitarsgarden.com#{a}" }.
   sort
 
+
+doc = Nokogiri::HTML(URI.open("https://guitarsgarden.com/collections/sg"))
+s_guitars = doc.search("a").
+  map {|a| a[:href]}.
+  compact.
+  select { |a| a.include?("ffst") }.
+  uniq.
+  map{ |a| "https://guitarsgarden.com#{a}" }.
+  sort
+
+guitars = lp_guitars + s_guitars
 puts guitars
 
 File.open("available_guitars.txt", "w") { |f| f.write guitars.join("\n") }
