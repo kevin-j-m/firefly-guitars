4bf8a3310e57150c858f1f650634effcf9d407e6
Kevin Murphy <murphy.kevin.mail@gmail.com>
2024-02-16 08:11:36 -0500

    pagination

--- firefly_guitars_for_sale.rb
+++ firefly_guitars_for_sale.rb
@@ -1,25 +1,40 @@
 require 'nokogiri'
 require 'open-uri'
 
-doc = Nokogiri::HTML(URI.open("https://guitarsgarden.com/collections/fflp-electric-guitars"))
+def guitars(site:, product_id:, page:)
+  page = Nokogiri::HTML(URI.open("#{site}?page=#{page}"))
 
-lp_guitars = doc.search("a").
-  map {|a| a[:href]}.
-  compact.
-  select { |a| a.include?("/collections/fflp-electric-guitars/products") }.
-  uniq.
-  map{ |a| "https://guitarsgarden.com#{a}" }.
-  sort
+  page.search("a")
+    .map { |a| a[:href] }
+    .compact
+    .delete_if { |a| a.match?("#{site}?page=") }
+    .select { |a| a.include?(product_id) }
+    .reject { |a| a.include?("?page=") }
+    .uniq
+    .map{ |a| "https://guitarsgarden.com#{a}" }
+end
 
+def search_products(site:, product_id:)
+  products = []
 
-doc = Nokogiri::HTML(URI.open("https://guitarsgarden.com/collections/sg"))
-s_guitars = doc.search("a").
-  map {|a| a[:href]}.
-  compact.
-  select { |a| a.include?("ffst") }.
-  uniq.
-  map{ |a| "https://guitarsgarden.com#{a}" }.
-  sort
+  1.step do |page|
+    products_in_page = guitars(site:, product_id:, page:).reject { |a| a == site }
+    break if products_in_page.empty? || page == 5
+
+    products += products_in_page
+  end
+
+  products.compact.sort
+end
+
+lp_guitars = search_products(
+  site: "https://guitarsgarden.com/collections/fflp-electric-guitars",
+  product_id: "fflp",
+)
+s_guitars = search_products(
+  site: "https://guitarsgarden.com/collections/sg",
+  product_id: "ffst",
+)
 
 guitars = lp_guitars + s_guitars
 puts guitars
